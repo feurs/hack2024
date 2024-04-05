09ec3754f7bd4d957ff1b9e4f34aee0974231217
IagorViarenic <128300684+IagorViarenic@users.noreply.github.com>
2024-04-05 23:44:40 +0200

    Update scripts.php

--- examples/basic-form/scripts.php
+++ examples/basic-form/scripts.php
@@ -1,1 +1,26 @@
+<?php
+class Data
+{
+    public string $firstName = '';
+    public string $lastName = '';
+    public string $address = 'data.txt';
 
+    public function getDataFromForm($firstName, $lastName): void
+    {
+        $this->firstName = $firstName !== null ? $firstName : '';
+        $this->lastName = $lastName !== null ? $lastName : '';
+    }
+
+    public function serializeDataFromForm(): void
+    {
+        $data = array(
+            'firstName' => $this->firstName,
+            'lastName' => $this->lastName,
+        );
+
+        $serializedData = serialize($data);
+        file_put_contents($this->address, $serializedData . PHP_EOL, FILE_APPEND);
+
+    }
+}
+?>
