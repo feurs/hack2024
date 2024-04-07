e37dea37ddba2bc4bac61c8568fcede8e21a07aa
IagorViarenic <128300684+IagorViarenic@users.noreply.github.com>
2024-04-07 06:28:54 +0200

    Update create_table_for_one_index_users.php

--- For_Igor_SQL_Code/create_table_for_one_index_users.php
+++ For_Igor_SQL_Code/create_table_for_one_index_users.php
@@ -9,8 +9,9 @@ $dbname = "userslog";
 
 // sql to create table
 $sql = "CREATE TABLE id_client (
-  id INT(6) NOT NULL
-  )";
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  index_column INT NOT NULL
+)";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
@@ -22,6 +23,19 @@ try {
     // use exec() because no results are returned
     $conn->exec($sql);
     echo "Table MyGuests created successfully";
+  
+    $new_sql = "UPDATE id_client SET index_column = $user_id WHERE id = 1";
+        try {
+            $new_conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password_sql);
+            // set the PDO error mode to exception
+            $new_conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // use exec() because no results are returned
+            $new_conn->exec($new_sql);
+            echo "Table MyGuests created successfully";
+
+        } catch(PDOException $e) {
+            echo $new_sql . "<br>" . $e->getMessage();
+        }
     
 } catch(PDOException $e) {
     echo $sql . "<br>" . $e->getMessage();
